de26189b0a63183f857c04f7d81648c7f2956232
Pavlov Ivan <ivan.pavlov@joy-dev.com>
2022-04-04 01:54:00 +0300

    Add 4, 5 items

--- scripts/Fotoalbum.js
+++ scripts/Fotoalbum.js
@@ -1,3 +1,11 @@
+window.onload = main
+
+function main() {
+    document.getElementById("fullPhotoDiv").addEventListener("click", () => {
+        document.getElementById("fullPhotoDiv").style.display = "none";
+    })
+}
+
 const fotosPaths = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 const titles = ['Водопад', 'Водопад 2', 'Замок', 'Зелень', 'Лампа', 'Лампа 2', 'Зеленый фон', 'Вид',
@@ -17,6 +25,11 @@ for (let i = 0; i < Math.ceil(titles.length / 4); i++) {
         let image = document.createElement('img');
         image.setAttribute('src', `images/${fotosPaths[currentElement]}.jpg`);
         image.setAttribute('title', titles[currentElement]);
+
+        image.addEventListener("click", () => {
+            document.getElementById("fullPhotoDiv").style.display = "block";
+            document.getElementById("fullPhotoImg").setAttribute('src', image.getAttribute('src'));
+        });
 
         let name = document.createElement('h3');
         name.innerHTML = titles[currentElement];
--- scripts/Validator.js
+++ scripts/Validator.js
@@ -1,58 +1,93 @@
 window.onload = main;
 
 function main() {
-    let form = document.getElementById("form1");
-    form.onsubmit = validate;
+    let fieldsToValidate = [
+        document.getElementById("fio"),
+        document.getElementById("email"),
+        document.getElementById("phone"),
+        document.getElementById("1"),
+        document.getElementById("2"),
+        document.getElementById("date")
+    ]
+
+    fieldsToValidate[0].addEventListener("focusout", () => {
+        validate(fieldsToValidate);
+    });
+    fieldsToValidate[1].addEventListener("focusout", () => {
+        validate(fieldsToValidate);
+    });
+    fieldsToValidate[2].addEventListener("focusout", () => {
+        validate(fieldsToValidate);
+    });
+    fieldsToValidate[3].addEventListener("focusout", () => {
+        validate(fieldsToValidate);
+    });
+    fieldsToValidate[4].addEventListener("focusout", () => {
+        validate(fieldsToValidate);
+    });
+    fieldsToValidate[5].addEventListener("focusout", () => {
+        validate(fieldsToValidate);
+    });
+
+    let button = document.getElementById("sendButton");
+    button.disabled = true;
 }
 
-function validate() {
-    let field1 = document.getElementById("fio");
-    let field2 = document.getElementById("email");
-    let phone = document.getElementById("phone");
-    let radio1 = document.getElementById("1");
-    let radio2 = document.getElementById("2");
-    let questionField = document.getElementById("question2");
+function validate(fields) {
+    let isValidateSuccessful = true;
 
-    if (field1.value.split(' ').length != 3) {
-        alert("Пожалуйста введите правильные данные");
-        field1.focus();
-        return false;
+    if (fields[0].value.split(' ').length != 3) {
+        fields[0].style.borderColor = "red";
+        document.getElementById("fioHint").style.display = "block";
+
+        isValidateSuccessful = false;
+    } else {
+        fields[0].style.borderColor = "green";
+        document.getElementById("fioHint").style.display = "none";
     }
-    if (field2 != null) {
-        if (field2.value == "") {
-            alert("Пожалуйста введите правильные данные");
-            field2.focus();
-            return false;
+
+    if (fields[1] != null) {
+        if (fields[1].value == "") {
+            fields[1].style.borderColor = "red";
+            isValidateSuccessful = false;
+            document.getElementById("emailHint").style.display = "block";
+        } else {
+            fields[1].style.borderColor = "green";
+            document.getElementById("emailHint").style.display = "none";
         }
     }
-    if (phone != null) {
-        if (!(phone.value.startsWith("+7") || phone.value.startsWith("+3")) ||
-            phone.value.includes(' ') || !(phone.value.length >= 9 && phone.value.length <= 11) || !/^\d+$/.test(phone.value.slice(1))) {
-            phone.focus();
-            alert("Пожалуйста введите правильные данные");
-            return false;
+
+    if (fields[2] != null) {
+        if (!(fields[2].value.startsWith("+7") || fields[2].value.startsWith("+3")) ||
+            fields[2].value.includes(' ') || !(fields[2].value.length >= 9 && fields[2].value.length <= 12) || !/^\d+$/.test(phone.value.slice(1))) {
+            fields[2].style.borderColor = "red";
+            isValidateSuccessful = false;
+            document.getElementById("phoneHint").style.display = "block";
+        } else {
+            fields[2].style.borderColor = "green";
+            document.getElementById("phoneHint").style.display = "none";
         }
     }
-    if (!radio1.checked && !radio2.checked) {
-        alert("Пожалуйста введите правильные данные");
-        return false;
+
+    if (!fields[3].checked && !fields[4].checked) {
+        isValidateSuccessful = false;
     }
 
-    if (questionField != null) {
-        let words = questionField.value.split(' ');
-        var isWordDigit = false;
-
-        words.forEach(element => {
-            if (/^\d+$/.test(element)) {
-                isWordDigit = true;
-            }
-        });
-
-        if (isWordDigit == false) {
-            questionField.focus();
-            alert("Пожалуйста введите правильные данные");
-            return false;
+    if (fields[5] != null) {
+        if (fields[5].value == "") {
+            fields[5].style.borderColor = "red";
+            isValidateSuccessful = false;
+            document.getElementById("dateHint").style.display = "block";
+        } else {
+            fields[5].style.borderColor = "green";
+            document.getElementById("dateHint").style.display = "none";
         }
     }
-    return true;
+
+    console.log(isValidateSuccessful);
+
+    let button = document.getElementById("sendButton");
+    button.disabled = !isValidateSuccessful;
+
+    return isValidateSuccessful;
 }
